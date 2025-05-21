588bebe94b2f7a280a43a77443946aadcfba820f
darvesh soni <darveshsoni1@gmail.com>
2025-05-21 10:48:40 +0530

    db

--- config/db.js
+++ config/db.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
     try{
        await mongoose.connect(process.env.MONGODB_URI)
-       console.log("")
-
+       console.log("mongodb connected sucessfully")
+    } catch (err) {
+ console.log(err,"connection failed");
     }
 }
+module.exports = connectDB;
