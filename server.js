5cc4451ce362a3c9a0a54014f184c06263e0b39f
darvesh soni <darveshsoni1@gmail.com>
2025-05-21 10:58:27 +0530

    imp

--- server.js
+++ server.js
@@ -1,3 +1,22 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const session = require("express-session")
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const path = require('path');
+const connectDB = require('./config/db');
+
+dotenv.config();
+const app = express();
+connectDB();
+
+//middleware
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static('public'));
+
+//session
+app.use(
+    session({
+        secret: process.env
+    })
+)
