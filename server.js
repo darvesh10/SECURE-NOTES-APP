be9635c8b6dc316cb751781fc5920e9cbba8098b
darvesh soni <darveshsoni1@gmail.com>
2025-05-21 11:19:51 +0530

    day 1 complete

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const session = require("express-session");
 const MongoStore = require("connect-mongo");
 const path = require('path');
 const connectDB = require('./config/db');
+const PORT = process.env.PORT || 3000
 
 dotenv.config();
 const app = express();
@@ -17,6 +18,22 @@ app.use(express.static('public'));
 //session
 app.use(
     session({
-        secret: process.env
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false,
+        store: MongoStore.create({mongoUrl: process.env.MONGODB_URI}),
     })
-)
+);
+
+// VIEW ENGINE
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+//routes
+app.get('/',(req, res)=>{
+    res.render('auth/login')
+});
+
+app.listen(PORT, ()=>{
+    console.log(`server is runing on ${PORT}`);
+})
